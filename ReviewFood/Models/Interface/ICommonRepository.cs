c1c59cd1d2e6affe811c3f69b3bc3a99c8a0b4db
Ashfaqul Haque <ashhaq12345@gmail.com>
2017-08-16 03:33:13 +0600

    Adding-  CommonRepository Interface  CommonManager Interface  Interface for -   Food Manager,   Food Repository,   Restaurant Manager,   Restaurant Repository,   Review Manager,   Review Repository.

---
+++ ReviewFood/Models/Interface/ICommonRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewFood.Models.Interface
+{
+    public interface ICommonRepository<T>: IDisposable where T:class
+    {
+        bool Add(T entity);
+
+        bool Update(T entity);
+
+        bool Remove(T entity);
+
+        T GetById(long id);
+
+        ICollection<T> GetAll();
+
+        bool Add(ICollection<T> entities);
+
+        bool Update(ICollection<T> entities);
+    }
+}
---
+++ ReviewFood/Models/Interface/IManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewFood.Models.Interface
+{
+    public interface IManager<T> where T:class
+    {
+        bool Add(T entity);
+
+        bool Update(T entity);
+
+        bool Remove(long id);
+
+        T GetById(long id);
+
+        ICollection<T> GetAll();
+    }
+}
